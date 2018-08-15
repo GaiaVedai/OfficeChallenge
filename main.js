d744a7e6f2faa81a5111f1ab5585711d6fa8d840
Gaia <you@example.com>
2018-08-15 17:22:26 +0300

    created general frame

--- main.js
+++ main.js
@@ -1,2 +1,54 @@
-//write your code here to make the tests pass
+class Office{
+    constructor() {
+        this.documents = [{}];
+        this.managers =[{}];
+        this.cleaners = {};
+        };
+    hireCleaner(name){
+    // a function that pushed a new cleaner to the cleaners array
+    } 
+    hireManager(name) {
+    // a function that pushed a new manager to the managers array        
+    }
+    startWorkDay() {
+    // a function that invokes the askEmployeesToWork function 
+    // for all the managers, and the clean function for all cleaners
+    }
+};
 
+class Document {
+    constructor(EmployeeName){
+        this.EmployeeName.toString() = EmployeeName 
+    }
+};
+
+class EmployeeName {
+    constructor() {
+
+    }
+    work(office) {
+    // a function - pushes 10 new documents to the office.documents[]
+
+    }
+    class Manager {
+    constructor(){
+        this.Employees = [];
+        this.name = managerName;
+        }
+        hireEmployee(EmployeeName) {
+            // a function that pushes a new employee to employees.array
+        }
+        askEmployeesToWork(office) {
+            // a function that invokes the employees work()
+        }
+};
+class Cleaner {
+    constructor() {
+    this.name = CleanerName
+}
+clean () {
+    //function that console.log "clean"
+}
+}
+
+}
